6f9f8814028b1dceab7b620143c62556372e0f2a
Rizdhan Hernanda <rizdhanhr@gmail.com>
2022-11-18 08:22:37 +0700

    belajar dasar

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_1/song_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SongPage(),
+    );
+  }
+}
---
+++ lib/song_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_1/neu_box.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class SongPage extends StatefulWidget {
+  const SongPage({Key? key}) : super(key: key);
+
+  @override
+  State<SongPage> createState() => _SongPageState();
+}
+
+class _SongPageState extends State<SongPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: NeuBox(child: Icon(Icons.arrow_back_ios)),
+                    ),
+                    Text('P L A Y L I S T'),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: NeuBox(child: Icon(Icons.menu)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25),
+                NeuBox(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset('lib/images/cover.jpg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Bullet For My Valentine',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.grey.shade700,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 6,
+                                ),
+                                const Text(
+                                  'Tears Dont Fall',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                              size: 32,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('0.00'),
+                    Icon(Icons.shuffle),
+                    Icon(Icons.repeat),
+                    Text('5:49')
+                  ],
+                ),
+                const SizedBox(height: 30),
+                NeuBox(
+                  child: LinearPercentIndicator(
+                    lineHeight: 10,
+                    percent: 0.5,
+                    progressColor: Colors.green,
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  height: 80,
+                  child: Row(
+                    children: const [
+                      Expanded(
+                        child: NeuBox(
+                            child: Icon(
+                          Icons.skip_previous,
+                          size: 32,
+                        )),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20.0),
+                          child: NeuBox(
+                              child: Icon(
+                            Icons.play_arrow,
+                            size: 32,
+                          )),
+                        ),
+                      ),
+                      Expanded(
+                        child: NeuBox(
+                            child: Icon(
+                          Icons.skip_next,
+                          size: 32,
+                        )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
